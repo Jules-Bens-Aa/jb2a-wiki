4fceebe296de68cbe37fa9aac30b5a7524375fb3
Jules-Bens-Aa <73002638+Jules-Bens-Aa@users.noreply.github.com>
2025-04-29 03:07:17 +0200

    other projects intro

--- astro.config.mjs
+++ astro.config.mjs
@@ -138,7 +138,9 @@ export default defineConfig({
                 label: 'Other Projects',
                 collapsed: true,
                 items: [
-                    'other-projects/token-animator-guide'
+                    'other-projects/other-projects-introduction',
+                    'other-projects/token-animator-guide',
+                    'other-projects/extras-free-module'
                 ]
             },
             {
